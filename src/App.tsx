c9bf02344b60fb4120d0a03cb0018192fe803cae
mdtahin <mmdtahin@gmail.com>
2026-09-11 18:54:54 +0600

    Empty files are ready for react assigment with tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-5xl text-red-500'>Hello</h1>
+      <button class="btn btn-primary">Button</button>
+    </>
+  )
+}
+
+export default App

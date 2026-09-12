fd53acb2ad9d8f2350fef004c63138ef7b044644
mdtahin <mmdtahin@gmail.com>
2026-09-13 03:02:07 +0600

    Final Adjustment for Mobile-View

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import Logo from "../assets/logo-text.png"
+import Bars from "../assets/bars logo.png"
 import '../index.css'
 
 const Nav = () => {
     return (
         <nav className='sticky top-0 z-50 flex navbar bg-amber-50 justify-between items-center container mx-auto px-4'>
-            <img src={Logo} alt="Logo in nav" />
+            <div className="flex items-center gap-4">
+                <img className='h-8 block md:hidden text-slate-800 focus:outline-none text-xl p-1" aria-label="Open Menu' src={Bars} alt="" />
+            </div>
 
-            <ul className='flex gap-6'>
+            <div className="absolute left-1/2 -translate-x-1/2 md:static md:translate-x-0">
+                <img className='h-8 object-contain' src={Logo} alt="Logo in nav" />
+            </div>
+
+            <ul className='hidden md:flex gap-6'>
                 <li className='hover:text-fuchsia-600 active:text-'>Home</li>
                 <li className='hover:text-fuchsia-600'>Technologies</li>
                 <li className='hover:text-fuchsia-600'>Projects</li>
